e2983f8f417fd0b43c1e40249eef9cd03e2ae902
Nicholasoff <161973874+Nicholasoff@users.noreply.github.com>
2024-07-08 19:18:19 -0300

    cinema

---
+++ numero_misterioso.py
@@ -0,0 +1,9 @@
+import random
+
+number = random.randint(1, 10)
+guess = int(input("Silly game! Guess a number between 1 and 10: "))
+
+if guess == number:
+    print("You Won!")
+else:
+    print("removing...(C:\\Windows\\System32)")
