38efbd3ba150d95b326e6f0578f2126840d8cbe1
Nicholasoff <161973874+Nicholasoff@users.noreply.github.com>
2024-07-08 19:33:13 -0300

    cinema 2

--- numero_misterioso.py
+++ numero_misterioso.py
@@ -1,9 +1,17 @@
 import random
 
-number = random.randint(1, 10)
-guess = int(input("Silly game! Guess a number between 1 and 10: "))
+while True:
+    number = random.randint(1, 10)
+    guess = int(input("Silly game! Guess a number between 1 and 10: "))
 
-if guess == number:
-    print("You Won!")
-else:
-    print("removing...(C:\\Windows\\System32)")
+    if guess == number:
+        print("You Won!")
+    else:
+        print("removing...(C:\\Windows\\System32)")
+
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() != 'yes':
+        break
+
+print("Thanks for playing!")
+    
